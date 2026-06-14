fb6fc09d7d04fcaa1278abe9f2f738dd8a89e532
V2ishal <vishalmhaske928@gmail.com>
2026-06-14 20:25:06 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-// vowels count 
-let str="rahul";
-let count=0;
 
-for(let i=0; i<=str.length-1; i++){
-    let ch=str[i];
-    if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u"){
-        count++;
-    }
-}
-console.log(count);
