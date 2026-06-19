92311743514325c0ca3924894ffab2aa91d3be10
V2ishal <vishalmhaske928@gmail.com>
2026-06-19 21:23:35 +0530

    string is palindrome or not solve using method

---
+++ app.js
@@ -0,0 +1,11 @@
+// string is palindrome or not using method
+let str="nayan";
+
+let reverse=str.split("").reverse().join("");
+
+if(str==reverse){
+    console.log("string is palindrome");
+}
+else{
+    console.log("string is not palindrome");
+}
