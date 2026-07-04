31e90f1f5fa06bb796cd2eeb6bdc92ed961094e8
V2ishal <vishalmhaske928@gmail.com>
2026-07-04 20:30:40 +0530

    app .js remove

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// string is palindrome or not
-let student="nayan";
 
-let palindrome="string is palindrome";
-
-for(let i=0; i<student.length/2; i++){
-    if(student[i]!==student[student.length-1-i]){
-        palindrome="string is not a palindrome";
-        break;
-    }
-}
-console.log(palindrome);
