50f0f15f88620a60e5c11421e77ba7a00b941203
V2ishal <vishalmhaske928@gmail.com>
2026-08-05 15:04:01 +0530

    string dublicate solved

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-  
+// string is palindrome or not using method
+let str="abbcd";
+let reverse="";
+
+for(let i=0; i<str.length; i++){
+  let dublicate="false"
+    for(let j=0; j<reverse.length; j++){
+         if(str[i]==reverse[j]){
+            dublicate="true";
+            break;
+         }
+    }
+    if(dublicate==="false"){
+        reverse+=str[i];
+    }
+}
+console.log(reverse);
