962769cf38cd5885fc44d8aedb223ecaff74299c
V2ishal <vishalmhaske928@gmail.com>
2026-08-14 15:09:59 +0530

    string compretion solved

---
+++ app.js
@@ -0,0 +1,15 @@
+// Q6 string compression 
+let str="aaabbc";  // a   a   a   b   b   c
+let result="";     // 0   1   2   3   4   5
+let count=1;
+for(let i=0; i<str.length; i++){
+    if(str[i]===str[i+1]){
+        count++;
+    }
+    else{
+        result+=str[i]+count;
+        count=1;
+    }
+}
+console.log(result);
+
