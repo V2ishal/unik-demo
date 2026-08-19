ee7ecbf5bb976e6cf68f0bf6b11a47fe1fea93b4
V2ishal <vishalmhaske928@gmail.com>
2026-08-19 14:17:45 +0530

    string comprestion solved

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ let str="aaabbc";  // a   a   a   b   b   c
 let result="";     // 0   1   2   3   4   5
 let count=1;
 for(let i=0; i<str.length; i++){
-    if(str[i]===str[i+1]){
+    while(str[i]===str[i+1]){
         count++;
+        i++;
     }
-    else{
-        result+=str[i]+count;
-        count=1;
-    }
+   result+=str[i]+count;
+      count=1;
 }
 console.log(result);
 
