ca53ba669aae02f3353105df47bdfc83f77b3fe3
V2ishal <vishalmhaske928@gmail.com>
2026-08-20 16:05:31 +0530

    app.js empty

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-// Q6 string compression 
-let str="aaabbc";  // a   a   a   b   b   c
-let result="";     // 0   1   2   3   4   5
-for(let i=0; i<str.length; i++){
-    let count=1;
-    while(str[i]===str[i+1]){
-        count++;
-        i++;
-    }
-   result+=str[i]+count;
-      
-}
-console.log(result);
+
 
