320221c177d5c4fc9b6cc2f96e2b6ca41ccee3f3
V2ishal <vishalmhaske928@gmail.com>
2026-08-25 15:08:04 +0530

    first non repeating char solved

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let result="";
 for(let i=0; i<str.length; i++){
     let count=0;
     for(let j=0; j<str.length; j++){
-          if(i!==j && str[i]===str[j]){
+          if(str[i]==str[j]){
             count++;
           }
     }
