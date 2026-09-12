ca67791eb55b4266d62aaa4db4b1eabd16b68ee8
V2ishal <vishalmhaske928@gmail.com>
2026-09-12 12:15:15 +0530

    first non repeating char solved

---
+++ app.js
@@ -0,0 +1,10 @@
+let str="swiss";
+let result="";
+
+for(let ch of str ){
+    if(str.indexOf(ch)===str.lastIndexOf(ch)){
+        result+=ch;
+        break;
+    }
+}
+console.log(result);                                                             
