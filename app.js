086cf6dcaca85eb16a5ded6746b58044efac4cfd
V2ishal <vishalmhaske928@gmail.com>
2026-09-16 10:34:51 +0530

    index.html and style.css empty

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-let str="swiss";
-let result="";
-
-for(let ch of str ){
-    if(str.indexOf(ch)===str.lastIndexOf(ch)){
-        result+=ch;
-        break;
-    }
-}
-console.log(result);                                                             
+                                                          
